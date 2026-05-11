a253af37f08bf75bb0b16faea8dce0700068e6df
nanecorner <daniel2000rincon@gmail.com>
2026-05-11 06:23:44 -0600

    deploy

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -3,7 +3,6 @@
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 import { createClient } from '@/lib/supabase/server'
-import { createAdminClient } from '@/lib/supabase/admin'
 
 export async function login(formData: FormData) {
   const supabase = await createClient()
