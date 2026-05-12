aadc1486083f574242b2c0e23834e0a783e10b83
nanecorner <daniel2000rincon@gmail.com>
2026-05-12 16:04:09 -0600

    callback

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -27,7 +27,7 @@ export async function resetPassword(formData: FormData) {
   const email = formData.get('email') as string
 
   const { error } = await supabase.auth.resetPasswordForEmail(email, {
-    redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'}/auth/reset-password`,
+    redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'}/auth/callback?next=/auth/reset-password`,
   })
 
   if (error) {
